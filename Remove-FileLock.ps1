9b6e5944c1faeb8b0f85641fb95c1d8a305431cc
egor-yudkin <egor.yudkin@gmail.com>
2016-10-13 10:12:28 +0300

    Added Remove-FileLock; RePublish-AlWorkflows

---
+++ Remove-FileLock.ps1
@@ -0,0 +1,16 @@
+﻿$web = get-spweb "http://srv-test-224.ponyex.local"
+$list = $web.Lists["MyListName"]
+$item = $list.Items.GetItemById(6)
+
+$userId = $item.File.LockedByUser.ID
+$user = $web.AllUsers.GetById($userId)
+
+$impSite = New-Object Microsoft.SharePoint.SPSite($web.Url, $user.UserToken);
+$impWeb = $impSite.OpenWeb();
+$impList = $impWeb.Lists[$list.Title];
+$impItem = $impList.GetItemById($item.ID);
+$impItem.File.ReleaseLock($impItem.File.LockId)
+
+$impWeb.Dispose()
+$impSite.Dispose()
+$web.Dispose()
