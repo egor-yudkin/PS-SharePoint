1607ae9e789c17de45aa47440c23a3c4dbed62a0
Egor Yudkin <egor-yudkin@users.noreply.github.com>
2018-01-18 14:30:41 -0500

    WorkflowProgressByState query now runs for 3 states

--- SQL/CountWorkflowProgressByState.sql
+++ SQL/CountWorkflowProgressByState.sql
@@ -1,7 +1,17 @@
 -- Get the number of WorkflowProgress records in DB based on State of WorkflowInstance
 -- 2 = 'Running'; 4 = 'Completed'; 8 = 'Cancelled'; 64 = 'Error'
 
-SELECT count(WP.WorkflowProgressID)
+SELECT count(WP.WorkflowProgressID) as Error
   FROM [dbo].[WorkflowInstance] as WFInst
   JOIN [dbo].[WorkflowProgress] as WP ON WFInst.InstanceID = WP.InstanceID
   WHERE WFInst.[State] = 64
+
+SELECT count(WP.WorkflowProgressID) as Cancelled
+  FROM [dbo].[WorkflowInstance] as WFInst
+  JOIN [dbo].[WorkflowProgress] as WP ON WFInst.InstanceID = WP.InstanceID
+  WHERE WFInst.[State] = 8
+
+  SELECT count(WP.WorkflowProgressID) as Completed
+  FROM [dbo].[WorkflowInstance] as WFInst
+  JOIN [dbo].[WorkflowProgress] as WP ON WFInst.InstanceID = WP.InstanceID
+  WHERE WFInst.[State] = 4
