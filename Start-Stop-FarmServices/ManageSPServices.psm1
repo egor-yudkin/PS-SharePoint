d75d6c54a7bdf444c8ac83f804a14f4ce6868868
Egor Yudkin <egor-yudkin@users.noreply.github.com>
2018-06-01 16:22:12 -0400

    Add files via upload

--- Start-Stop-FarmServices/ManageSPServices.psm1
+++ Start-Stop-FarmServices/ManageSPServices.psm1
@@ -1,168 +1,166 @@
-# Functions to manage SharePoint-related Windows services across farm servers.
-# Author: Egor Yudkin
-# Modified: 12/27/2017
+<#
+.SYNOPSIS
+  Functions to manage SharePoint-related Windows services across farm servers.
+
+.INPUTS
+  None
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        2.0
+  Author:         Egor Yudkin
+  Creation Date:  03/05/2018
+  Purpose/Change: Added functions for Search-related services
+                  Cleaned up the code
+                  Improved compliance with code standards
+
+  Version:        1.0
+  Author:         Egor Yudkin
+  Creation Date:  12/27/2017
+  Purpose/Change: Initial development
+
+#>
+
+#Requires -Version 3
+#Requires -PSSnapin Microsoft.SharePoint.PowerShell
+
+Function StartStop-WindowsService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ServiceName,
+
+        [ValidateSet("Start", "Stop", "Restart")]
+        [string]
+        $Action,
+
+        [parameter()]
+        [string]
+        $ServiceTitle,
+
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+
+    if (!$ServerNames) 
+    {
+        $ServerNames = @($env:COMPUTERNAME)
+    }
+    if ($null -eq $ServiceTitle -or $ServiceTitle -eq [string]::Empty)
+    {
+        $ServiceTitle = $ServiceName
+    }
+
+	foreach ($server in $ServerNames) 
+    {
+        try
+        {
+            $service = Get-Service -ComputerName $server -Name $ServiceName -ErrorAction Stop
+        }
+        catch
+        {
+            Write-Warning "Service '$ServiceName' was not found on $server"
+            continue
+        }
+
+        try 
+        {
+            switch ($Action)
+            {
+                "Start"
+                {
+                    Write-Verbose "Starting '$ServiceTitle' service on $server"
+                    Start-Service -InputObject $service -ErrorAction Stop
+                }
+                "Stop"
+                {
+                    Write-Verbose "Stopping '$ServiceTitle' service on $server"
+                    Stop-Service -InputObject $service -ErrorAction Stop
+                }
+                "Restart"
+                {
+                    Write-Verbose "Restarting '$ServiceTitle' service on $server"
+                    Restart-Service -InputObject $service -ErrorAction Stop
+                }
+            }
+	        
+        }
+        catch
+        {
+            Write-Warning "Service '$ServiceName' failed to $Action"
+        }
+	}
+}
+
+Function Set-WindowsServiceState
+{
+}
+
+Function Set-WindowsServiceStartupType
+{
+}
 
 Function Restart-SPTimerService
 {
     [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
     ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Restarting SharePoint Timer service on $server"
-	     Restart-Service -InputObject (Get-Service -ComputerName $server -Name "sptimerv4")
-	    }
+	if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
     }
+    StartStop-WindowsService -ServiceName "sptimerv4" -ServiceTitle "SharePoint Timer" -Action Restart -ServerNames $ServerNames
 }
 
 Function Stop-SPTimerService
 {
     [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
     ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Stopping SharePoint Timer service on $server"
-	     Stop-Service -InputObject (Get-Service -ComputerName $server -Name "sptimerv4")
-	    }
+	if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
     }
+	StartStop-WindowsService -ServiceName "sptimerv4" -ServiceTitle "SharePoint Timer" -Action Stop -ServerNames $ServerNames
 }
 
 Function Start-SPTimerService
 {
     [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
     ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Starting SharePoint Timer service on $server"
-	     Start-Service -InputObject (Get-Service -ComputerName $server -Name "sptimerv4")
-	    }
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
     }
-}
-
-Function Restart-IISService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Restarting W3WP service on $server"
-	     Restart-Service -InputObject (Get-Service -ComputerName $server -Name "w3svc")
-	    }
-    }
-}
-
-Function Stop-IISService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Stopping W3WP service on $server"
-	     Stop-Service -InputObject (Get-Service -ComputerName $server -Name "w3svc")
-	    }
-    }
-}
-
-Function Start-IISService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Starting W3WP service on $server"
-	     Start-Service -InputObject (Get-Service -ComputerName $server -Name "w3svc")
-	    }
-    }
-}
-
-Function Restart-SPAdminService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Restarting SharePoint Administration service on $server"
-	     Restart-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4")
-	    }
-    }
-}
-
-Function Stop-SPAdminService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Stopping SharePoint Administration service on $server"
-	     Stop-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4")
-	    }
-    }
-}
-
-Function Start-SPAdminService
-{
-    [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
-    ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Starting SharePoint Administration service on $server"
-	     Start-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4")
-	    }
-    }
+	StartStop-WindowsService -ServiceName "sptimerv4" -ServiceTitle "SharePoint Timer" -Action Start -ServerNames $ServerNames
 }
 
 Function Enable-SPTimerService
 {
     [CmdletBinding()]
-    #region PARAMS
     param (
         [string[]]$ServerNames
     ) 
-    #endregion PARAMS
     process {
 	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
 	    foreach ($server in $ServerNames) {
@@ -175,63 +173,305 @@ Function Enable-SPTimerService
 Function Disable-SPTimerService
 {
     [CmdletBinding()]
-    #region PARAMS
     param (
         [string[]]$ServerNames
     ) 
-    #endregion PARAMS
     process {
 	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
 	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Enabling SharePoint Timer service on $server"
+	     Write-Verbose "Disabling SharePoint Timer service on $server"
 	     Set-Service -InputObject (Get-Service -ComputerName $server -Name "sptimerv4") -StartupType Disabled
 	    }
     }
 }
 
-Function Enable-SPAdminService
+Function Restart-IISService
 {
     [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
     ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Enabling SharePoint Administration service on $server"
-	     Set-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4") -StartupType Automatic
-	    }
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
     }
+    StartStop-WindowsService -ServiceName "w3svc" -ServiceTitle "IIS" -Action Restart -ServerNames $ServerNames
+}
+
+Function Stop-IISService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "w3svc" -ServiceTitle "IIS" -Action Stop -ServerNames $ServerNames
+}
+
+Function Start-IISService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "w3svc" -ServiceTitle "IIS" -Action Start -ServerNames $ServerNames
+}
+
+Function Restart-SPAdminService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "spadminv4" -ServiceTitle "SharePoint Administration" -Action Restart -ServerNames $ServerNames
+}
+
+Function Stop-SPAdminService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "spadminv4" -ServiceTitle "SharePoint Administration" -Action Stop -ServerNames $ServerNames
+}
+
+Function Start-SPAdminService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "spadminv4" -ServiceTitle "SharePoint Administration" -Action Start -ServerNames $ServerNames
+}
+
+Function Enable-SPAdminService
+{
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+	if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
+	foreach ($server in $ServerNames) {
+	    Write-Verbose "Enabling SharePoint Administration service on $server"
+	    Set-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4") -StartupType Automatic
+	}
 }
 
 Function Disable-SPAdminService
 {
     [CmdletBinding()]
-    #region PARAMS
-    param (
-        [string[]]$ServerNames
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+	if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
+	foreach ($server in $ServerNames) {
+	    Write-Verbose "Disabling SharePoint Administration service on $server"
+	    Set-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4") -StartupType Disabled
+	}
+}
+
+Function Stop-SPSearchService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
     ) 
-    #endregion PARAMS
-    process {
-	    if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
-	    foreach ($server in $ServerNames) {
-	     Write-Verbose "Enabling SharePoint Administration service on $server"
-	     Set-Service -InputObject (Get-Service -ComputerName $server -Name "spadminv4") -StartupType Disabled
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "OSearch15" -ServiceTitle "SharePoint Server Search" -Action Stop -ServerNames $ServerNames
+}
+
+Function Start-SPSearchService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "OSearch15" -ServiceTitle "SharePoint Server Search" -Action Start -ServerNames $ServerNames
+}
+
+Function Enable-SPSearchService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+	if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
+	foreach ($server in $ServerNames) {
+	    Write-Verbose "Enabling SharePoint Search service on $server"
+	    Set-Service -InputObject (Get-Service -ComputerName $server -Name "OSearch15") -StartupType Automatic
+	}
+}
+
+Function Disable-SPSearchService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+	if (!$ServerNames) {$ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name}
+	foreach ($server in $ServerNames) {
+	    Write-Verbose "Disabling SharePoint Search service on $server"
+	    Set-Service -InputObject (Get-Service -ComputerName $server -Name "OSearch15") -StartupType Disabled
+	}
+}
+
+Function Stop-SPSearchHostService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "SPSearchHostController" -ServiceTitle "SharePoint Search Host Controller" -Action Stop -ServerNames $ServerNames
+}
+
+Function Start-SPSearchHostService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    ) 
+    if (!$ServerNames) 
+    {
+        $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+    }
+    StartStop-WindowsService -ServiceName "SPSearchHostController" -ServiceTitle "SharePoint Search Host Controller" -Action Start -ServerNames $ServerNames
+}
+
+Function Enable-SPSearchHostService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+    process 
+    {
+	    if (!$ServerNames) 
+        {
+            $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+        }
+	    foreach ($server in $ServerNames) 
+        {
+	        Write-Verbose "Enabling SharePoint Search Host Controller service on $server"
+	        Set-Service -InputObject (Get-Service -ComputerName $server -Name "SPSearchHostController") -StartupType Automatic
 	    }
     }
 }
 
+Function Disable-SPSearchHostService
+{
+    [CmdletBinding()]
+    param 
+    (
+        [parameter()]
+        [string[]]
+        $ServerNames
+    )
+    process 
+    {
+	    if (!$ServerNames) 
+        {
+            $ServerNames = (Get-SPServer | ?{$_.Role -ne [Microsoft.SharePoint.Administration.SPServerRole]::Invalid}).Name
+        }
+	    foreach ($server in $ServerNames) 
+        {
+	        Write-Verbose "Disabling SharePoint Search Host Controller service on $server"
+	        Set-Service -InputObject (Get-Service -ComputerName $server -Name "SPSearchHostController") -StartupType Disabled
+	    }
+    }
+}
 
-<# 
-Export-ModuleMember Restart-TimerServices,
-                    Stop-TimerServices, 
-                    Start-TimerServices, 
-                    Restart-IISServices, 
-                    Stop-IISServices, 
-                    Start-IISServices, 
-                    Restart-SPAdminServices, 
-                    Stop-SPAdminServices, 
-                    Start-SPAdminServices
-                    #>
+Export-ModuleMember Restart-SPTimerService,
+                    Stop-SPTimerService, 
+                    Start-SPTimerService,
+                    Enable-SPTimerService,
+                    Disable-SPTimerService, 
+                    Restart-IISService, 
+                    Stop-IISService, 
+                    Start-IISService, 
+                    Restart-SPAdminService, 
+                    Stop-SPAdminService, 
+                    Start-SPAdminService,
+                    Enable-SPAdminService,
+                    Disable-SPAdminService,
+                    Stop-SPSearchHostService, 
+                    Start-SPSearchHostService,
+                    Enable-SPSearchHostService,
+                    Disable-SPSearchHostService,
+                    Stop-SPSearchService, 
+                    Start-SPSearchService,
+                    Enable-SPSearchService,
+                    Disable-SPSearchService
